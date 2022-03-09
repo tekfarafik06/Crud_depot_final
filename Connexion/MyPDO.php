33b863a76db6809ebe75fadbe6cea89a11d8cb9d
Angelo <angelo@osradar.com>
2022-03-09 21:51:18 +0100

    Connexion/MyPDO.php

--- Connexion/MyPDO.php
+++ Connexion/MyPDO.php
@@ -10,6 +10,8 @@ include "VariableDsn.php";
 
 class MyPDO
 {
+
+
     /**
      * @var PDO
      */
@@ -56,6 +58,8 @@ class MyPDO
      */
     public function __construct($sgbd, $host, $db, $user, $password, $nomTable)
     {
+
+
         switch ($sgbd) {
             case "mysql":
                 $this->pdo = new PDO("mysql:host=" . $host . ";dbname=" . $db, $user, $password);
@@ -96,6 +100,7 @@ class MyPDO
      */
     public function getAll(): array
     {
+
         if (!isset($this->pdos_selectAll))
             $this->initPDOS_selectAll();
         $this->getPdosSelectAll()->execute();
@@ -104,6 +109,11 @@ class MyPDO
             "natation\Entite" . ucfirst($this->getNomTable())
         );
     }
+
+
+
+
+
 
     /**
      * préparation de la requête SELECT * FROM $this->nomTable WHERE $nomColId = :id
@@ -122,6 +132,18 @@ class MyPDO
      * @param $val
      * @return mixed
      */
+
+    public function getParticipe($val1, $val2)
+    {
+        if (!isset($this->pdos_select)) {
+            $requete = "SELECT * FROM " . $this->nomTable . " WHERE id_nageur = $val1 AND id_epreuve = $val2;";
+            $this->pdos_select = $this->pdo->prepare($requete);
+            $this->pdos_select->execute();
+            return $this->getPdosSelect()
+                ->fetchObject("natation\Entite" . ucfirst($this->getNomTable()));
+        }
+    }
+
     public function get($key, $val)
     {
         if (!isset($this->pdos_select))
@@ -137,6 +159,8 @@ class MyPDO
      * @param string $nomColId
      * @param array $colNames
      */
+
+
     public function initPDOS_update(string $nomColId, array $colNames): void
     {
         $query = "UPDATE " . $this->nomTable . " SET ";
@@ -145,6 +169,18 @@ class MyPDO
         }
         $query = substr($query, 0, strlen($query) - 2);
         $query .= " WHERE " . $nomColId . "=:" . $nomColId;
+        $this->pdos_update =  $this->pdo->prepare($query);
+    }
+
+    public function initPDOS_updateParticipe(string $nomColId, string $nomColId2,  array $colNames): void
+    {
+        $query = "UPDATE " . $this->nomTable . " SET ";
+        foreach ($colNames as $colName) {
+            $query .= $colName . "=:" . $colName . ", ";
+        }
+        $query = substr($query, 0, strlen($query) - 2);
+        $query .= " WHERE " . $nomColId . "=:" . $nomColId.
+            " AND " . $nomColId2 . "=:" . $nomColId2;
         $this->pdos_update =  $this->pdo->prepare($query);
     }
 
@@ -162,6 +198,32 @@ class MyPDO
         $this->getPdosUpdate()->execute();
     }
 
+    public function updateParticipe($id, $id2, array $assoc): void
+    {
+        /*$query = "";
+        if (!isset($this->pdos_select)) {
+            $query = "UPDATE participe SET  ";//. " WHERE id_nageur = $val1 AND id_epreuve = $val2;";
+            $cpt = 0;
+
+            foreach ($assoc as $elt) {
+                $query .= key($elt) . " = $elt";
+                if ($cpt < count($assoc) - 1) $requete .= ",";
+            }
+            $query .= " where  id_nageur = $id_nageur , id_epreuve = $id_epreuve ;";
+        }
+        echo $query;
+        $query = $this->pdo->prepare($query);
+        $query->execute();*/
+
+        if (!isset($this->pdos_update))
+            $this->initPDOS_updateParticipe($id, $id2, array_keys($assoc));
+        foreach ($assoc as $key => $value) {
+            $this->getPdosUpdate()->bindValue(":" . $key, $value);
+        }
+        $this->getPdosUpdate()->execute();
+
+    }
+
     /**
      * @param array
      */
@@ -171,8 +233,10 @@ class MyPDO
         foreach ($colNames as $colName) {
             $query .= ":" . $colName . ", ";
         }
+
         $query = substr($query, 0, strlen($query) - 2);
         $query .= ')';
+        //echo $query;
         $this->pdos_insert = $this->pdo->prepare($query);
     }
 
@@ -187,6 +251,48 @@ class MyPDO
             $this->getPdosInsert()->bindValue(":" . $key, $value);
         }
         $this->getPdosInsert()->execute();
+    }
+
+    public function countParticipants(int $id1,int $id2)
+    {
+        $this->verifierLiaisonParticipe( $id1, $id2);
+    }
+
+    public function verifierLiaisonParticipe(int $id1,int $id2){
+        $requete= 'select count(*) FROM nageur as n INNER JOIN participe as p on(n.id_nageur = p.id_nageur)
+                    INNER JOIN  epreuve as e ON(e.id_epreuve = p.id_epreuve)
+                    where n.id_nageur ='.$id1.' AND e.id_epreuve ='.$id2.';';
+        $this->pdos_count = $this->pdo->prepare($requete);
+
+        /*$this->pdos_select = $this->pdo->prepare($requete);
+        echo $requete;*/
+        //$requete= $this->pdo->prepare($requete);
+
+        //return $this->pdos_select->execute()->fetch(PDO::FETCH_NUM)[0];;
+    }
+    public function insertParticipe(array $assoc): void
+    {
+        $requete ="";
+        if (!isset($this->pdos_select)) {
+            /*$requete = "INSERT INTO
+                        participe(id_nageur , id_epreuve , nom_medaille , classement , date_epreuve , performance); ";/* . " WHERE id_nageur = $val1 AND id_epreuve = $val2;";
+            */
+            $requete = "INSERT INTO  
+                        participe VALUES(";/* . " WHERE id_nageur = $val1 AND id_epreuve = $val2;";*/
+
+            $cpt = 0;
+
+            foreach ($assoc as $elt) {
+                $virgule = "";
+                if($cpt > 0) $virgule = ", ";
+                $requete .= $virgule . '"'.$elt.'"';
+                $cpt++;
+            }
+            $requete.=")";
+        }
+        echo $requete;
+        $requete= $this->pdo->prepare($requete);
+        $requete->execute();
     }
 
     /**
@@ -211,6 +317,15 @@ class MyPDO
         $this->getPdosDelete()->execute();
     }
 
+
+    public function deleteParticipe($id1, $id2)
+    {
+        $requete = "delete FROM participe WHERE id_nageur = $id1 AND id_epreuve = $id2;";
+
+        $query = $this->pdo->prepare($requete);
+        $query->execute();
+    }
+
     /**
      * préparation de la requête SELECT COUNT(*) FROM livre
      * instantiation de self::$_pdos_count
@@ -225,12 +340,12 @@ class MyPDO
     }
 
 
-
     public function getCountValue(): int
     {
         $this->count();
         return $this->pdos_count->fetch(PDO::FETCH_NUM)[0];
     }
+
     /**
      * @return PDO
      */
